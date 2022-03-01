f40bcd955457fcbc9db9cda6bb2c2f86c6695f6a
Prajyot Dhotre <63930185+prajyot48@users.noreply.github.com>
2022-02-28 19:20:49 -0700

    Time: 85 ms, Memory: 16.1 MB - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,27 +6,34 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        res = []
-        q=deque()
-        q.append(root)
-        while q:
-            qlen = len(q)
-            level=[]
-            for i in range(qlen):
-                node = q.popleft()
-                if node:
-                    level.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
-            if level:
-                    res.append(level)
-        # print(res)
-        return(len(res))
-        # def Maxi(root):  
-        #     if (root == None):
-        #         return 0
-        #     else:
-        #         left = Maxi(root.left)
-        #         right = Maxi(root.right)
-        #         return max(left,right)+1
-        # return(Maxi(root))
+        #//using bfs
+        # res = []
+        # q=deque([root])
+        # # q.append(root)
+        # level=0
+        # while q:
+        #     qlen = len(q)
+        #     level=[]
+        #     for i in range(qlen):
+        #         node = q.popleft()
+        #         level.append(node.val)
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     if level:
+        #         res.append(level)
+        #     print(res)
+        # return(level)
+        
+        
+        def Maxi(root):  
+            if (root == None):
+                return 0
+            else:
+                left = Maxi(root.left)
+                # print("LEFT",root.val,left)
+                right = Maxi(root.right)
+                # print("RIGHT",root.val,right)
+                return max(left,right)+1
+        return(Maxi(root))
